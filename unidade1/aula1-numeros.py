c6a61718d82476f1a615d906b7a0926919162554
retatu <mrehbein45@gmail.com>
2018-10-17 19:46:09 -0300

    First Commit

---
+++ unidade1/aula1-numeros.py
@@ -0,0 +1,11 @@
+print("hello")
+a = 5
+print(a)
+#Tipo Inteiro
+print(type(a))
+#Divisão por 3
+print(a/3)
+#Tipo Float
+print(type(a/3))
+#Expoent
+print(a**2)
---
+++ unidade1/aula2-strings.py
@@ -0,0 +1,9 @@
+#String
+print('Olá')
+#tipo String
+print(type('Ola'))
+#Aspas duplas para poder utilizar aspas simples dentro
+print("Olá'''")
+print('Olá"""')
+#len apresenta o tamanho
+print(len("tamanho"))
