ad4073abe2d931e50af8208e9408e1fcdbbad85c
retatu <mrehbein45@gmail.com>
2018-10-18 07:28:52 -0300

    Adicionado indexes das strings

--- unidade1/aula2-strings.py
+++ unidade1/aula2-strings.py
@@ -7,3 +7,25 @@ print("Olá'''")
 print('Olá"""')
 #len apresenta o tamanho
 print(len("tamanho"))
+
+s = 'Valor dentro de uma variavel'
+print(s)
+#retorna o valor na posicao
+print(s[3])
+#retorna os valores da string entre as posições
+print(s[3:])
+print(s[:3])
+print(s[2:3])
+#com o valor negativo, começa de tras
+print(s[-4:-2])
+print(s[-1:])
+print(s[:-4])
+
+#intervalo entre os valores, por padrão é 1
+print(s[::2])
+print(s[::3])
+#pega de trás para frente
+print(s[::-1])
+print(s[::-3])
+
+
