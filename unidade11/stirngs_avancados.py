de67babece58b985431afa1df655c63af73de09f
retatu <mrehbein45@gmail.com>
2018-11-19 07:20:09 -0200

    adicionado metodos avançados string

---
+++ unidade11/stirngs_avancados.py
@@ -0,0 +1,17 @@
+string = "string para teste. 123 testando"
+print(string.capitalize())
+print(string.upper())
+print(string.lower())
+#Adiciona as letras equivalentes dos lados...s
+print(string.center(40, 'a'))
+#Conta a quantidade da letra na string
+print(string.find('a'))
+#Se é tudo números
+print(string.isalnum())
+#Se é tudo alfabético
+print(string.isalpha())
+print(string.islower())
+print(string.isupper())
+print(string.endswith('o'))
+print(string.split('p'))
+print(string.partition('p'))
