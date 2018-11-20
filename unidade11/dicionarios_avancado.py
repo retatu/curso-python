e81790f4ca081dd8cd7be20408f2f65f4edb389f
retatu <mrehbein45@gmail.com>
2018-11-20 07:18:24 -0200

    Adicionado dicionarios avançado

---
+++ unidade11/dicionarios_avancado.py
@@ -0,0 +1,12 @@
+x = {x:x**2 for x in range(10)}
+print(x)
+
+d = {'k1':1,'k2':2}
+for key in d.keys():
+    print(key)
+
+for value in d.values():
+    print(value)
+
+for item in d.items():
+    print(item)
