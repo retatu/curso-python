446726cd8caaa8211d98db620a94ad407cee08ab
retatu <mrehbein45@gmail.com>
2018-11-20 07:15:18 -0200

    Adicionado conjuntos(set) avancados

---
+++ unidade11/conjuntos_avancados.py
@@ -0,0 +1,20 @@
+s = set()
+s.clear()
+s = {1,2,3}
+s2 = s.copy()
+s2.add(4)
+s2.add(5)
+print(s2.difference(s))
+#Adiciona no s apenas os valores diferentes dos dois sets
+s2.difference_update(s)
+print(s2)
+s2 = {3,4,5}
+s = {1,2,3}
+print(s.intersection(s2))
+#Adiciona no s apenas os valores iguais dos dois sets
+s.intersection_update(s2)
+print(s)
+
+s2 = {3,4,5}
+s = {1,2,3}
+print(s.union(s2))
