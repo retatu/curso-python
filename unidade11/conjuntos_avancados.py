358f22965481f700bb027bd8503c6065f01e55bd
retatu <mrehbein45@gmail.com>
2018-11-21 07:15:29 -0200

    Corrigido conjutos_avancados que tinha sido substituido o conteudo interno

--- unidade11/conjuntos_avancados.py
+++ unidade11/conjuntos_avancados.py
@@ -1,17 +1,19 @@
-string = "string para teste. 123 testando"
-print(string.capitalize())
-print(string.upper())
-print(string.lower())
-#Adiciona as letras equivalentes dos lados...s
-print(string.center(40, 'a'))
-#Conta a quantidade da letra na string
-print(string.find('a'))
-#Se é tudo números
-print(string.isalnum())
-#Se é tudo alfabético
-print(string.isalpha())
-print(string.islower())
-print(string.isupper())
-print(string.endswith('o'))
-print(string.split('p'))
-print(string.partition('p'))
+s = set()
+s.clear()
+s = {1,2,3}
+s2 = s.copy()
+s2.add(4)
+s2.add(5)
+print(s2.difference(s))
+#Adiciona no s apenas os valores diferentes dos dois sets
+s2.difference_update(s)
+print(s2)
+s2 = {3,4,5}
+s = {1,2,3}
+print(s.intersection(s2))
+#Adiciona no s apenas os valores iguais dos dois sets
+s.intersection_update(s2)
+print(s)
+ s2 = {3,4,5}
+s = {1,2,3}
+print(s.union(s2))
